454c89802a702b5a5a1bb104b0bd60e968c691fa
dxwts <dxwts@onyx-international.com>
2013-12-13 09:38:23 +0800

    create project

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,47 @@
+# coding: utf-8
+require "digest/md5"
+module UsersHelper
+   def user_avatar_width_for_size(size)
+    case size
+      when :normal then 48
+      when :small then 24
+      when :large then 64
+      when :big then 120
+      else size
+    end
+  end
+  
+  def user_avatar_size_name_for_2x(size)
+    case size
+    when :normal then :large
+    when :small then :normal
+    when :large then :big
+    when :big then :big
+    else size
+    end
+  end
+
+  def user_avatar_tag(user, size = :normal, opts = {})
+    link = opts[:link] || true
+
+    width = user_avatar_width_for_size(size)
+
+    if user.blank?
+      return image_tag("avatar/#{size}.png")
+    end
+
+    if user[:avatar].blank?
+      default_url = asset_path("avatar/#{size}.png")
+      img_src = "http://gravatar.com/avatar/#{user.email_md5}.png?s=#{width * 2}&d=404"
+      img = image_tag(img_src, :class => "uface", :style => "width:#{width}px;height:#{width}px;")
+    else
+      img = image_tag(user.avatar.url(user_avatar_size_name_for_2x(size)), :style => "width:#{width}px;height:#{width}px;")
+    end
+
+    if link
+      raw %(<a href="">#{img}</a>)
+    else
+      raw img
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnyxWeekly::Application.config.secret_key_base = '77516b0d0035e7b821adb99225d82abed09ef20d824d4a7c364c63d6d2dbd66b6758ef631243a153cefa90072164f35a45db84da5503a21ad83c409e889292e0'
