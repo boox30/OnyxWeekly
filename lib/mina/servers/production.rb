b5be3684cb0f956047a6d793c2ac936d5aaff490
dxwts <dxwts@onyx-international.com>
2013-12-16 16:46:21 +0800

    modify server handy name

--- lib/mina/servers/production.rb
+++ lib/mina/servers/production.rb
@@ -9,7 +9,7 @@ namespace :env do
     set :rvm_path,            '$HOME/.rvm/scripts/rvm'   # we don't use that. see below.
     set :services_path,       '/etc/init.d'                  # where your God and Unicorn service control scripts will go
     set :nginx_path,          '/etc/nginx'
-    set :deploy_server,       'bookstore'                    # just a handy name of the server
+    set :deploy_server,       'onyxweekly'                    # just a handy name of the server
     set :unicorn_workers,     2
     invoke :defaults                                         # load rest of the config
     invoke :"rvm:use[ruby-2.0.0-p247@default]"                       # since my prod server runs 1.9 as default system ruby, there's no need to run rvm:use
